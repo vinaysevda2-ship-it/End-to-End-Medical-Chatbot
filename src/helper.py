da128f45d47ab0bd35744cfe65d9ec5251531ae9
vinaysevda2-ship-it <vinaysevda2@gmail.com>
2026-05-11 18:34:52 +0530

    fixed cicd pipeline

--- src/helper.py
+++ src/helper.py
@@ -1,8 +1,8 @@
-from langchain.document_loaders import PyPDFLoader, DirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings import HuggingFaceEmbeddings
 from typing import List
-from langchain.schema import Document
+from langchain.schema import Document   
+from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain_community.embeddings import HuggingFaceEmbeddings
 
 
 
